043386d2d212a13ad66537db99a6910984063a91
Destaran <abonyibalazs95@gmail.com>
2023-08-07 19:38:43 +0200

    increased Calculator Container width

--- src/routes/Calculator/Calculator.component.jsx
+++ src/routes/Calculator/Calculator.component.jsx
@@ -8,7 +8,7 @@ import { ItemTree } from "../../components/Calculator/ItemTree/ItemTree.componen
 export const Container = styled.div`
   margin: 0px auto;
   padding: 30px;
-  width: 80%;
+  width: 85%;
   height: 100%;
   background-color: #f1f1f1;
   user-select: none;
