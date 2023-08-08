38ba890c589907a7f8ac90e5263eb3064809ca29
Destaran <abonyibalazs95@gmail.com>
2023-08-08 03:45:51 +0200

    initialized Profile component

--- src/routes/Profile/Profile.component.jsx
+++ src/routes/Profile/Profile.component.jsx
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import { UserContext } from "../../contexts/user.context";
 
-
+const Container = styled.div`
+  margin: 0px auto;
+  padding: 30px;
+  width: 80%;
+  height: 100%;
+  background-color: #f1f1f1;
+  user-select: none;
+`;
 
 const Profile = () => {
-    return (
-        <h1>This is the Profile page</h1>
-    )
+  const { currentUser } = useContext(UserContext);
+  return (
+    <Container>
+      <h1>Welcome {currentUser.displayName}!</h1>
+    </Container>
+  );
 };
 
 export default Profile;
