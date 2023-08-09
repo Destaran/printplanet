2720aa8965a4794d1e7885ac00d19185a31adcf1
Destaran <abonyibalazs95@gmail.com>
2023-08-09 14:51:46 +0200

    removed LoginForm.styles

--- src/components/LoginForm/LoginForm.component.jsx
+++ src/components/LoginForm/LoginForm.component.jsx
@@ -1,12 +1,4 @@
-import {
-  LoginContainer,
-  InnerLoginContainer,
-  Form,
-  OrContainer,
-  GoogleContainer,
-  GoogleButton,
-  PasswordReset,
-} from "./LoginForm.styles";
+import styled from "styled-components";
 import {
   signInWithGooglePopup,
   signInAuthUserWithEmailAndPassword,
@@ -17,6 +9,70 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/user.context";
 import FormInput from "../FormInput/FormInput.component";
 import { Button } from "../Button/Button.component";
+import { DefaultButton } from "../Button/Button.component";
+
+const Container = styled.div`
+  margin: 30px auto;
+  padding: 0;
+  width: 300px;
+  border: 1px black solid;
+  background-color: #f1f1f1;
+  border-radius: 10px;
+`;
+
+const InnerLoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 25px;
+`;
+
+const Form = styled.form`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+const OrContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  color: black;
+
+  p {
+    margin: 0px;
+  }
+`;
+
+const GoogleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const GoogleButton = styled(DefaultButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  img {
+    width: 15px;
+    height: auto;
+    padding-right: 10px;
+  }
+`;
+
+const PasswordReset = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  margin: 0;
+  height: 15px;
+
+  p:hover {
+    cursor: pointer;
+    color: orange;
+  }
+`;
 
 const defaultFormFields = {
   email: "",
@@ -71,7 +127,7 @@ const LoginForm = () => {
   };
 
   return (
-    <LoginContainer>
+    <Container>
       <InnerLoginContainer>
         <Form action="">
           <FormInput
@@ -105,7 +161,7 @@ const LoginForm = () => {
           <p onClick={handleForgotPassword}>Forgot password?</p>
         </PasswordReset>
       </InnerLoginContainer>
-    </LoginContainer>
+    </Container>
   );
 };
 
