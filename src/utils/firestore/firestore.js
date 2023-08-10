3c476b7de38f5607dab8e749b121131560e1cb9b
Destaran <abonyibalazs95@gmail.com>
2023-08-11 01:35:37 +0200

    added getUserDoc to firestore.js

--- src/utils/firestore/firestore.js
+++ src/utils/firestore/firestore.js
@@ -103,6 +103,17 @@ export const createUserDocumentFromAuth = async (
   return userDocRef;
 };
 
+export const getUserDocument = async (uid) => {
+  const docRef = doc(db, "users", uid);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.log("No such document!");
+  }
+};
+
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
