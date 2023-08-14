f3fc50b6e2eea23a05434c1670ccda61df4e1f99
Destaran <abonyibalazs95@gmail.com>
2023-08-14 19:09:04 +0200

    added guide page

---
+++ src/routes/Guide/Guide.component.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  margin: 0px auto;
+  padding: 30px;
+  width: 85%;
+  height: 100%;
+  background-color: #f1f1f1;
+  user-select: none;
+`;
+
+export const Guide = () => {
+  return (
+    <Container>
+      <h1>This is the Guide page</h1>
+    </Container>
+  );
+};
