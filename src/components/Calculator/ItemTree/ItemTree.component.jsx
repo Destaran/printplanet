6318f5f0d84a1941fec8dd0e711930cbb995e2b7
Destaran <abonyibalazs95@gmail.com>
2023-08-15 01:39:25 +0200

    added animation to ItemTree

--- src/components/Calculator/ItemTree/ItemTree.component.jsx
+++ src/components/Calculator/ItemTree/ItemTree.component.jsx
@@ -1,9 +1,20 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { useSelector } from "react-redux";
 import { calculatedOutput } from "../../../reduxStore/calculator/calculator.selector";
 import { TreeDiv } from "./TreeDiv/TreeDiv.component";
 
-const Container = styled.div``;
+const MountAnimation = keyframes`
+from {
+  opacity: 0;
+}
+to {
+  opacity: 1;
+}
+`;
+
+const Container = styled.div`
+  animation: ${MountAnimation} 0.5s ease-out;
+`;
 
 const Title = styled.p`
   margin: 0 0 1px 6px;
