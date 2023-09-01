5368c0f68e3a29dd77f6ad41a8bcd2ddb1b76468
Destaran <abonyibalazs95@gmail.com>
2023-09-01 03:53:23 +0200

    finished ChangePassPopup functionality

--- src/components/Profile/ChangePassPopup/ChangePassPopup.component.tsx
+++ src/components/Profile/ChangePassPopup/ChangePassPopup.component.tsx
@@ -3,6 +3,10 @@ import { Popup } from "../../Popup/Popup.component";
 import { FormInput } from "../../FormInput/FormInput.component";
 import { Button } from "../../Button/Button.component";
 import { useState } from "react";
+import {
+  setNewPassword,
+  signInAuthUserWithEmailAndPassword,
+} from "../../../utils/firestore/firestore";
 
 const ButtonsContainer = styled.div`
   display: flex;
@@ -13,54 +17,102 @@ const ButtonsContainer = styled.div`
   }
 `;
 
+const ErrorMessage = styled.p`
+  margin: 0 auto;
+  color: red;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
 interface Props {
+  email: string;
   setPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export const ChangePassPopup = ({ setPopup }: Props) => {
+export const ChangePassPopup = ({ email, setPopup }: Props) => {
   const [formFields, setFormFields] = useState({
+    oldPass: "",
     newPass: "",
     newConfirm: "",
   });
-  const { newPass, newConfirm } = formFields;
+  const [error, setError] = useState<string | null>(null);
+  const { oldPass, newPass, newConfirm } = formFields;
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   };
 
-  const handleSubmit = () => {
-    //
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (newPass !== newConfirm) {
+      setError("Passwords do not match!");
+      return;
+    }
+    if (newPass.length < 6) {
+      setError("Password should be at least 6 characters long!");
+      return;
+    }
+    try {
+      await signInAuthUserWithEmailAndPassword(email, oldPass);
+      await setNewPassword(newPass);
+      setError(null);
+      setPopup(false);
+    } catch (error: any) {
+      if (error.code == "auth/wrong-password") {
+        setError("Wrong password!");
+        return;
+      }
+      setError("An error occured!");
+    }
   };
 
   const handleCancel = () => {
+    setError(null);
     setPopup(false);
   };
 
   return (
     <Popup title="Set New Password">
-      <FormInput
-        required
-        type="password"
-        value={newPass}
-        onChange={handleChange}
-        label="New Password"
-        name="newPass"
-      />
-      <FormInput
-        required
-        type="password"
-        value={newConfirm}
-        onChange={handleChange}
-        label="Confirm Password"
-        name="newConfirm"
-      />
+      <Form>
+        <FormInput
+          required
+          type="password"
+          value={oldPass}
+          onChange={handleChange}
+          label="Old Password"
+          name="oldPass"
+        />
+        <FormInput
+          required
+          type="password"
+          value={newPass}
+          onChange={handleChange}
+          label="New Password"
+          name="newPass"
+        />
+        <FormInput
+          required
+          type="password"
+          value={newConfirm}
+          onChange={handleChange}
+          label="Confirm Password"
+          name="newConfirm"
+        />
+      </Form>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <ButtonsContainer>
         <Button buttonType={"green"} onClick={handleSubmit}>
-          <u>E</u>nter
+          Enter
         </Button>
         <Button buttonType={"red"} onClick={handleCancel}>
-          <u>B</u>ack
+          Back
         </Button>
       </ButtonsContainer>
     </Popup>
