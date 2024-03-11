17d052c54629c7b1306608316f7fc57a2ac108c2
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-11 17:53:26 +0100

    update auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  controllers: [],
+  providers: [JwtStrategy],
+  exports: [PassportModule],
 })
 export class AuthModule {}
