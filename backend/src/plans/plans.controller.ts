c176e5278d6fa91814741753d83c64e867cb61ca
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-21 00:21:56 +0100

    feat: add basic plans service and controller

--- backend/src/plans/plans.controller.ts
+++ backend/src/plans/plans.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  UseGuards,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { PlansService } from './plans.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('plans')
 export class PlansController {
   constructor(private readonly plansService: PlansService) {}
 
   @Get()
-  async findAll() {
-    return await this.plansService.findAll();
+  findAll() {
+    return this.plansService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.plansService.find(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  create(@Body('plan') plan) {
+    return this.plansService.create(plan);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  update(@Param('id') id, @Body('plan') plan) {
+    return this.plansService.update(id, plan);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.plansService.delete(id);
   }
 }
--- backend/src/plans/plans.service.ts
+++ backend/src/plans/plans.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PlansService {
-  async findAll() {
+  findAll() {
     return 'This action returns all plans';
+  }
+
+  find(id: number) {
+    return `This action returns the plan with id: ${id}`;
+  }
+
+  create(plan) {
+    return `This action creates a new plan`;
+  }
+
+  update(id: number, plan) {
+    return `This action updates the plan with id: ${id}`;
+  }
+
+  delete(id: number) {
+    return `This action removes the plan with id: ${id}`;
   }
 }
