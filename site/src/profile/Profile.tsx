4db4a10a63f990082757b91b2dffa539a8631d3f
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-27 00:50:35 +0100

    feat: add user to profile for testing

--- site/src/profile/Profile.tsx
+++ site/src/profile/Profile.tsx
@@ -1,9 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { PageBase } from "../components/PageBase";
 
 export const Profile = () => {
+  const { user } = useAuth0();
+
+  if (!user) {
+    return null;
+  }
+  console.log(user);
+
   return (
     <PageBase>
-      <h1></h1>
+      <h1>Hi {user.nickname}!</h1>
     </PageBase>
   );
 };
