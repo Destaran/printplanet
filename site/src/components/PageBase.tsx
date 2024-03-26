727e631cdc6fdf5ba3bed13aa074c930fce182b0
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-26 22:08:35 +0100

    feat: add PageBase component

---
+++ site/src/components/PageBase.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { ReactNode } from "react";
+import { ppLightGrey } from "../utils/colors";
+
+const Container = styled.div`
+  background-color: ${ppLightGrey};
+  margin: auto;
+  justify-content: center;
+  width: 65%;
+  align-items: start;
+  flex-grow: 1;
+  padding: 20px;
+`;
+
+interface PageBaseProps {
+  children?: ReactNode;
+}
+
+export function PageBase({ children }: PageBaseProps) {
+  return <Container>{children}</Container>;
+}
