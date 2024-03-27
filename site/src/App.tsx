27b4dec2ee318ff31223f972b26684d01a7cbfdf
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-27 11:09:37 +0100

    chore: refactor App routes

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -2,17 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-import { Routes, Route } from "react-router-dom";
-import { Navigation } from "./navigation/Navigation";
-import { LandingPage } from "./landing-page/LandingPage.component";
-import { Calculator } from "./calculator/Calculator";
-import { Guide } from "./guide/Guide";
-import { About } from "./about/About.component";
 import styled from "styled-components";
-import { Profile } from "./profile/Profile";
-import { AuthGuard } from "./AuthGuard";
+import { AppRoutes } from "./AppRoutes";
 
-const Wrapper = styled.div`
+const PageWrapper = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -23,20 +16,9 @@ export const App = () => {
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Wrapper>
-            <Routes>
-              <Route path="/" element={<Navigation />}>
-                <Route index element={<LandingPage />} />
-                <Route path="guide" element={<Guide />} />
-                <Route path="calculator" element={<Calculator />} />
-                <Route path="about" element={<About />} />
-                <Route
-                  path="profile"
-                  element={<AuthGuard component={Profile} />}
-                />
-              </Route>
-            </Routes>
-          </Wrapper>
+          <PageWrapper>
+            <AppRoutes />
+          </PageWrapper>
         </PersistGate>
       </Provider>
     </BrowserRouter>
---
+++ site/src/AppRoutes.tsx
@@ -0,0 +1,22 @@
+import { AuthGuard } from "./AuthGuard";
+import { About } from "./about/About.component";
+import { Calculator } from "./calculator/Calculator";
+import { Guide } from "./guide/Guide";
+import { LandingPage } from "./landing-page/LandingPage.component";
+import { Navigation } from "./navigation/Navigation";
+import { Profile } from "./profile/Profile";
+import { Route, Routes } from "react-router-dom";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<LandingPage />} />
+        <Route path="guide" element={<Guide />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<AuthGuard component={Profile} />} />
+      </Route>
+    </Routes>
+  );
+}
