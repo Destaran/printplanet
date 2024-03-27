520946210a286416a914539e764cc429a2fdfbf9
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-03-27 11:20:41 +0100

    feat: add GlobalStyle and theme to App

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import styled, { ThemeProvider } from "styled-components";
 import { store, persistor } from "./redux/store";
-import styled from "styled-components";
 import { AppRoutes } from "./AppRoutes";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { theme } from "./styles/theme";
 
 const PageWrapper = styled.div`
   min-height: 100vh;
@@ -13,14 +15,19 @@ const PageWrapper = styled.div`
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <PageWrapper>
-            <AppRoutes />
-          </PageWrapper>
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+              <PageWrapper>
+                <AppRoutes />
+              </PageWrapper>
+            </PersistGate>
+          </Provider>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 };
