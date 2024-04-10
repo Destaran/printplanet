94127846c094f3fc96b5efb9ce579c09d940067b
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-04-10 18:05:29 +0200

    feat: improve useDisplayNumber

--- site/src/utils/useDisplayNumber.ts
+++ site/src/utils/useDisplayNumber.ts
@@ -2,9 +2,9 @@ import numeral from "numeral";
 
 export function useDisplayNumber(number: number) {
   let formatted = numeral(number).format("0.00a");
-  if (formatted.split(".")[1][1] === "0") {
+  if (formatted.split(".")[1][1] === "0" || formatted.length > 5) {
     formatted = numeral(number).format("0.0a");
-    if (formatted.split(".")[1][0] === "0") {
+    if (formatted.split(".")[1][0] === "0" || formatted.length > 5) {
       formatted = numeral(number).format("0a");
     }
   }
