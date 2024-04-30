037877bd5ae76b87dcb2b076091d1b7e5b79b793
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-04-30 20:44:46 +0200

    chore: remove header

--- site/src/navigation/Header.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  text-align: center;
-`;
-
-export function Header() {
-  return <Title>PrintPlanet</Title>;
-}
--- site/src/navigation/Navigation.tsx
+++ site/src/navigation/Navigation.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./NavBar";
-import { Header } from "./Header";
 
 export function Navigation() {
   return (
     <>
-      <Header />
       <NavBar />
       <Outlet />
     </>
