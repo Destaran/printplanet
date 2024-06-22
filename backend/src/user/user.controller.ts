44e8bd8ea4b449063b9a04523be1fc83fa5ec07b
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-06-22 23:03:10 +0200

    feat: add basic user controller & service

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  find(@Param('id') id: number) {
+    return this.userService.find(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  create(@Body('plan') user) {
+    return this.userService.create(user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id')
+  update(@Param('id') id, @Body('user') user) {
+    return this.userService.update(id, user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.userService.delete(id);
+  }
+}
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  findAll() {
+    return 'This action returns all users';
+  }
+
+  find(id: number) {
+    return `This action returns the user with id: ${id}`;
+  }
+
+  create(user) {
+    return `This action creates a new user`;
+  }
+
+  update(id: number, user) {
+    return `This action updates the user with id: ${id}`;
+  }
+
+  delete(id: number) {
+    return `This action removes the user with id: ${id}`;
+  }
+}
