881ba2976681e2cd73197ce34c005c25c7a7a294
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-11 20:59:38 +0200

    feat: add checkUser to UserService

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,9 +2,19 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 
+interface Auth0User {
+  sub: string;
+  email: string;
+  nickname: string;
+}
+
 @Injectable()
 export class UserService {
   constructor(private readonly usersRepository: UserRepository) {}
+
+  create(user: User) {
+    return this.usersRepository.save(user);
+  }
 
   findAll() {
     return this.usersRepository.find();
@@ -14,15 +24,24 @@ export class UserService {
     return this.usersRepository.findOneBy({ id });
   }
 
-  create(user: User) {
-    return this.usersRepository.save(user);
-  }
-
   update(id: string, user: User) {
     return this.usersRepository.update(id, user);
   }
 
   delete(id: string) {
     return this.usersRepository.delete(id);
+  }
+
+  checkUser(user: Auth0User) {
+    const exists = this.usersRepository.findOneBy({ id: user.sub });
+    if (!exists) {
+      const newUser = {
+        id: user.sub,
+        email: user.email,
+        nickname: user.nickname,
+      };
+
+      this.create(newUser);
+    }
   }
 }
