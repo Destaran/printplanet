8c38242636fd0662938aa00e1dd89fd285b82416
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-12 11:24:06 +0200

    feat: foldable sections first implementation

--- site/src/calculator/Calculator.tsx
+++ site/src/calculator/Calculator.tsx
@@ -4,14 +4,25 @@ import { ItemSelect } from "./ItemSelect/ItemSelect";
 import { Summary } from "./Summary/Summary";
 import { ItemTree } from "./ItemTree/ItemTree";
 import { PageBase } from "../components/PageBase";
+import { CalculatorSection } from "./CalculatorSection";
 
 export function Calculator() {
   const output = useSelector(outputValues);
   return (
     <PageBase>
-      <ItemSelect />
-      {output.length > 0 && <Summary />}
-      {output.length > 0 && <ItemTree />}
+      <CalculatorSection name="Item Select">
+        <ItemSelect />
+      </CalculatorSection>
+      {output.length > 0 && (
+        <>
+          <CalculatorSection name="Summary">
+            <Summary />
+          </CalculatorSection>
+          <CalculatorSection name="Item Tree">
+            <ItemTree />
+          </CalculatorSection>
+        </>
+      )}
     </PageBase>
   );
 }
---
+++ site/src/calculator/CalculatorSection.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+const Header = styled.button`
+  padding: 5px;
+  margin: 0;
+  font-size: 1em;
+  font-weight: bold;
+  width: 100%;
+  text-align: left;
+  margin-bottom: 15px;
+  border-radius: 0px;
+  background-color: ${({ theme }) => theme.colors.orange};
+  color: ${({ theme }) => theme.colors.black};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.darkOrange};
+    color: ${({ theme }) => theme.colors.white};
+    cursor: pointer;
+  }
+`;
+
+interface Props {
+  children: React.ReactNode;
+  name: string;
+}
+
+export function CalculatorSection({ children, name }: Props) {
+  const [active, setActive] = useState(true);
+
+  function handleClick() {
+    setActive(!active);
+  }
+
+  return (
+    <>
+      <Header onClick={handleClick}>
+        {active ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}
+        {name}
+      </Header>
+      {active && children}
+    </>
+  );
+}
