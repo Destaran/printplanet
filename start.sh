1fea7e21d42a5bdc4a016fcacd9cf317b225eeac
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-12 12:02:34 +0200

    feat: improve docker compose

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q)
-
 yarn install
 yarn install --cwd ./backend
 yarn install --cwd ./site
 
-docker compose up --build
+docker compose up --build --force-recreate
 
