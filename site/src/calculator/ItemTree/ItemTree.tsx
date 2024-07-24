e729d9748364c3146a1b43a98c8087ff6fcd843b
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-24 10:50:22 +0200

    chore: remove redundant anim

--- site/src/calculator/ItemTree/ItemTree.tsx
+++ site/src/calculator/ItemTree/ItemTree.tsx
@@ -1,32 +1,16 @@
-import styled, { keyframes } from "styled-components";
 import { useSelector } from "react-redux";
 import { calculatedOutput } from "../../redux/calculator/calculator.selector";
 import { TreeDiv } from "./TreeDiv";
-
-const MountAnimation = keyframes`
-from {
-  opacity: 0;
-}
-to {
-  opacity: 1;
-}
-`;
-
-const Container = styled.div`
-  animation: ${MountAnimation} 0.5s ease-out;
-`;
-
-const Title = styled.p``;
 
 export function ItemTree() {
   const output = useSelector(calculatedOutput);
 
   return (
-    <Container>
-      <Title>Tree Overview</Title>
+    <div>
+      <p>Tree Overview</p>
       {output.map((outputItem, idx) => (
         <TreeDiv outputItem={outputItem} key={idx} />
       ))}
-    </Container>
+    </div>
   );
 }
