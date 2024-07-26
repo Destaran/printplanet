b748b4264d816be6f370f06334922e75741849d2
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-26 19:29:49 +0200

    style: tab styling

--- site/src/calculator/ItemTree/ItemTree.tsx
+++ site/src/calculator/ItemTree/ItemTree.tsx
@@ -7,8 +7,8 @@ import { getImageUrlById } from "utils/helperFunctions";
 import styled from "styled-components";
 
 const TabIcon = styled.img`
-  width: 32px;
-  height: 32px;
+  width: 36px;
+  height: 36px;
 `;
 
 const StyledTabs = styled(Tabs)``;
@@ -19,12 +19,48 @@ const StyledTabList = styled(TabList)`
 
 const StyledTab = styled(Tab)`
   display: inline-block;
-  border: 1px solid transparent;
-  border-bottom: none;
+  border-left: 4px solid;
   position: relative;
   list-style: none;
-  padding: 6px 12px;
+  padding: 3px 6px;
   cursor: pointer;
+  border-color: ${({ theme }) => theme.colors.darkOrange};
+  background-color: ${({ theme }) => theme.colors.grey};
+
+  img {
+    filter: brightness(0.8);
+  }
+
+  &:hover {
+    border-color: ${({ theme }) => theme.colors.orange};
+  }
+
+  &.selected {
+    border-color: ${({ theme }) => theme.colors.orange};
+    background-color: ${({ theme }) => theme.colors.blue};
+
+    img {
+      filter: brightness(1);
+    }
+  }
+
+  transition: all 1s;
+
+  &:hover {
+    transition: all 0.2s;
+    background-color: ${({ theme }) => theme.colors.blue2};
+    img {
+      filter: brightness(1);
+    }
+  }
+
+  &:active {
+    transition: all 0.2s;
+    background-color: ${({ theme }) => theme.colors.grey};
+    img {
+      filter: brightness(1);
+    }
+  }
 `;
 
 export function ItemTree() {
@@ -32,7 +68,7 @@ export function ItemTree() {
 
   return (
     <div>
-      <StyledTabs>
+      <StyledTabs selectedTabClassName="selected">
         <StyledTabList>
           {output.map((outputItem) => {
             const img = getImageUrlById(outputItem.id);
