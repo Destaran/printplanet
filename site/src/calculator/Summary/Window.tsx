62333e88e36b5ee8572dd16fbe401d28590798ec
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-26 14:50:29 +0200

    fix: Window overflow

--- site/src/calculator/Summary/Window.tsx
+++ site/src/calculator/Summary/Window.tsx
@@ -3,48 +3,28 @@ import { WindowItem } from "./WindowItem";
 import { OutputItem, SummaryItem } from "utils/types";
 
 const Container = styled.div`
-  min-width: auto;
-  height: auto;
-  width: auto;
-  display: flex;
-  justify-content: start;
-`;
-
-const WindowContainer = styled.div`
   width: 100%;
   height: auto;
   min-width: auto;
   margin-bottom: 10px;
 `;
 
-const WindowOutter = styled.div`
-  display: inline-flex;
+const WrapperWrapper = styled.div`
+  overflow-x: auto;
+  overflow-y: hidden;
+  white-space: nowrap;
+  background-color: ${({ theme }) => theme.colors.blue};
+`;
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(44px, 1fr));
   height: auto;
   align-items: center;
   justify-content: center;
-  border: 1px black solid;
-  background-color: ${({ theme }) => theme.colors.blue};
   margin: 0;
   padding: 4px;
   max-width: 100%;
-`;
-
-const WindowInner = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, 1fr);
-  grid-auto-flow: column;
-  border: 1px black solid;
-  background-color: ${({ theme }) => theme.colors.grey};
-  max-width: 100%;
-  height: auto;
-`;
-
-const TitleContainer = styled.div`
-  font-size: 1em;
-  width: 100%;
-  text-align: left;
-  border-radius: 0px;
-  color: ${({ theme }) => theme.colors.black};
 `;
 
 interface Props {
@@ -56,20 +36,16 @@ interface Props {
 export function Window({ title, items, handleClick }: Props) {
   return (
     <Container>
-      <WindowContainer>
-        <TitleContainer>
-          <p>{title}</p>
-        </TitleContainer>
-        <WindowOutter>
-          <WindowInner>
-            {items.map((item, idx) => {
-              return (
-                <WindowItem item={item} key={idx} handleClick={handleClick} />
-              );
-            })}
-          </WindowInner>
-        </WindowOutter>
-      </WindowContainer>
+      <p>{title}</p>
+      <WrapperWrapper>
+        <Wrapper>
+          {items.map((item, idx) => {
+            return (
+              <WindowItem item={item} key={idx} handleClick={handleClick} />
+            );
+          })}
+        </Wrapper>
+      </WrapperWrapper>
     </Container>
   );
 }
