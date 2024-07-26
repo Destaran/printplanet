227130ee3db7e8d676729d3616970ef68bb1546d
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-26 10:43:59 +0200

    feat: add tabs to ItemTree

--- site/src/calculator/ItemTree/ItemTree.tsx
+++ site/src/calculator/ItemTree/ItemTree.tsx
@@ -1,16 +1,38 @@
 import { useSelector } from "react-redux";
 import { calculatedOutput } from "../../redux/calculator/calculator.selector";
 import { TreeDiv } from "./TreeDiv";
+import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { getImageUrlById } from "utils/helperFunctions";
+import styled from "styled-components";
+
+const TabIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
 
 export function ItemTree() {
   const output = useSelector(calculatedOutput);
 
   return (
     <div>
-      <p>Tree Overview</p>
-      {output.map((outputItem, idx) => (
-        <TreeDiv outputItem={outputItem} key={idx} />
-      ))}
+      <Tabs>
+        <TabList>
+          {output.map((outputItem) => {
+            const img = getImageUrlById(outputItem.id);
+            return (
+              <Tab key={outputItem.id}>
+                <TabIcon src={img} />
+              </Tab>
+            );
+          })}
+        </TabList>
+        {output.map((outputItem) => (
+          <TabPanel>
+            <TreeDiv outputItem={outputItem} key={outputItem.id} />
+          </TabPanel>
+        ))}
+      </Tabs>
     </div>
   );
 }
