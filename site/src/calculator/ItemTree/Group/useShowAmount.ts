042cf7fe8cc2f3b3f13e5d1e11686b5f4a211919
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-07-27 00:57:46 +0200

    fix: useShowAmount useDisplayNumber

--- site/src/calculator/ItemTree/Group/useShowAmount.ts
+++ site/src/calculator/ItemTree/Group/useShowAmount.ts
@@ -1,7 +1,10 @@
 import numeral from "numeral";
 
-export function useShowAmount(num: number) {
-  let amount = numeral(num).format("0.00");
+export function useShowAmount(num: number): string {
+  if (num >= 1000) {
+    return Math.ceil(num).toString();
+  }
+  let amount = numeral(num).format("0.000");
   while (
     (amount.includes(".") && amount[amount.length - 1] === "0") ||
     amount[amount.length - 1] === "."
--- site/src/utils/hooks/useDisplayNumber.ts
+++ site/src/utils/hooks/useDisplayNumber.ts
@@ -1,19 +1,28 @@
 import numeral from "numeral";
 
-export function useDisplayNumber(number: number) {
+export function useDisplayNumber(number: number): string {
   let indicator = null;
   let formatted = numeral(number).format("0.0000a");
   if (isNaN(Number(formatted[formatted.length - 1]))) {
     indicator = formatted[formatted.length - 1];
     formatted = formatted.slice(0, -1);
   }
+
   while (
-    (indicator && formatted.includes(".")) ||
     (formatted.includes(".") && formatted[formatted.length - 1] === "0") ||
     formatted[formatted.length - 1] === "."
   ) {
     formatted = formatted.slice(0, -1);
   }
+
+  if (formatted.length > 4) {
+    formatted = formatted.slice(0, -(formatted.length - 4));
+  }
+
+  if (formatted[formatted.length - 1] === ".") {
+    formatted = formatted.slice(0, -1);
+  }
+
   if (indicator) {
     formatted += indicator;
   }
