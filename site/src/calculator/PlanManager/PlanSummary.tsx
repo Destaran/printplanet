9716711a080d24c142266c2c5b341390cf53b9f1
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-08-11 23:14:15 +0200

    fix: css and loading plan

--- site/src/calculator/PlanManager/PlanSummary.tsx
+++ site/src/calculator/PlanManager/PlanSummary.tsx
@@ -2,16 +2,28 @@ import styled from "styled-components";
 import { Plan } from "utils/API/plan/Plan";
 import { SummaryItem } from "utils/types";
 import { ItemIcon } from "./ItemIcon";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { loadPlan } from "redux/calculator/calculator.slice";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  justify-content: center;
+  align-items: start;
+  width: 80px;
+  border: 1px solid black;
+  margin: 5px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 const Title = styled.p``;
@@ -21,14 +33,20 @@ interface Props {
 }
 
 export function PlanSummary({ plan }: Props) {
+  const dispatch = useDispatch();
   const output = JSON.parse(plan.output);
 
+  const handleClick = useCallback(() => {
+    const planOutput = JSON.parse(plan.plan);
+    dispatch(loadPlan(planOutput));
+  }, []);
+
   return (
-    <Container>
+    <Container onClick={handleClick}>
       <Title>{plan.name}</Title>
       <Wrapper>
         {output.map((item: SummaryItem) => {
-          return <ItemIcon item={item} />;
+          return <ItemIcon item={item} key={item.id} />;
         })}
       </Wrapper>
     </Container>
