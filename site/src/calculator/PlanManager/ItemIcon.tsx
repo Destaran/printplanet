da43bf638351dcb0edbd244d222e802d3de62540
Balázs Abonyi <abonyibalazs95@gmail.com>
2024-08-11 23:14:30 +0200

    style: ItemIcon

--- site/src/calculator/PlanManager/ItemIcon.tsx
+++ site/src/calculator/PlanManager/ItemIcon.tsx
@@ -1,12 +1,40 @@
 import styled from "styled-components";
 import { getImageUrlById } from "utils/helperFunctions";
+import { useDisplayNumber } from "utils/hooks/useDisplayNumber";
 import { SummaryItem } from "utils/types";
 
 const Container = styled.div``;
 
+const InnerContainer = styled.div`
+  display: flex;
+  height: 100%;
+  width: 100%;
+`;
+
+const ImgContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  object-fit: contain;
+  background-color: ${({ theme }) => theme.colors.grey};
+`;
+
 const Image = styled.img`
-  width: 46px;
-  height: 46px;
+  height: 36px;
+  width: auto;
+`;
+
+const AmountText = styled.p`
+  position: absolute;
+  font-size: 14px;
+  bottom: -3px;
+  right: -1px;
+  margin: 0;
+  color: white;
+  text-shadow: 0px 1px 1px #000, 0px -1px 1px #000, 1px 0px 1px #000,
+    -1px 0px 1px #000;
 `;
 
 interface Props {
@@ -14,10 +42,16 @@ interface Props {
 }
 
 export function ItemIcon({ item }: Props) {
-  const img = getImageUrlById(item.id);
+  const imgUrl = getImageUrlById(item.id);
+  const displayAmount = useDisplayNumber(item.amount);
   return (
     <Container>
-      <Image src={img} alt={item.id} />
+      <InnerContainer>
+        <ImgContainer>
+          <Image src={imgUrl} />
+          {displayAmount && <AmountText>{displayAmount}</AmountText>}
+        </ImgContainer>
+      </InnerContainer>
     </Container>
   );
 }
